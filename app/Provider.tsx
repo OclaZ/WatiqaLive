bffd114b0e4e62a3b9efbd80f5ecfe1f7dadb641
OclaZ <aslikhhamza2001@gmail.com>
2024-09-25 05:49:16 +0100

    first commit

---
+++ app/Provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import { LiveblocksProvider } from "@liveblocks/react/suspense";
+import { ClientSideSuspense } from "@liveblocks/react/suspense";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return <div>PROVIDER</div>;
+  <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
+    <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
+  </LiveblocksProvider>;
+};
+
+export default Provider;
